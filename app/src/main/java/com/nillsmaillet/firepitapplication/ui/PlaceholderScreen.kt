44b398cf355a8f43b8e7ff9eefde924af7e72264
Nills Maillet <mailletnills@gmail.com>
2026-09-21 11:16:44 +0200

    feat: add reusable placeholder screen w/ preview

---
+++ app/src/main/java/com/nillsmaillet/firepitapplication/ui/PlaceholderScreen.kt
@@ -0,0 +1,25 @@
+package com.nillsmaillet.firepitapplication.ui
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun PlaceholderScreen(title: String) {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center,
+    ) {
+        Text(text = title)
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun PlaceholderScreenPreview() {
+    PlaceholderScreen(title = "Bibliothèque")
+}
