706f11b971506f1a854332e0fbcc82e55affc391
Nills Maillet <mailletnills@gmail.com>
2026-09-26 18:05:21 +0200

    style: add Firepit color palette and theme with a serif typography

--- app/src/main/java/com/nillsmaillet/firepitapplication/ui/theme/Color.kt
+++ app/src/main/java/com/nillsmaillet/firepitapplication/ui/theme/Color.kt
@@ -2,10 +2,14 @@ package com.nillsmaillet.firepitapplication.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val Primary = Color(0xFFB5523C)
+val Secondary = Color(0xFF8C3B2E)
+val Accent = Color(0xFF3F5E4A)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val BackgroundPrimary = Color(0xFF1F1B17)
+val SurfacePrimary = Color(0xFF2E2925)
+
+val TextPrimary = Color(0xFFF5EDE7)
+val TextSecondary = Color(0xFFB7A99A)
+
+val ErrorRed = Color(0xFFE57373)
