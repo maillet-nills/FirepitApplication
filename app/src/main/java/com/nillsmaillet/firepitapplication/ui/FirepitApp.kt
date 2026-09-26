a3bdcd064541727c033ad60558494190013da86f
Nills Maillet <mailletnills@gmail.com>
2026-09-26 10:59:50 +0200

    feat: add NavigationBarItems

--- app/src/main/java/com/nillsmaillet/firepitapplication/ui/FirepitApp.kt
+++ app/src/main/java/com/nillsmaillet/firepitapplication/ui/FirepitApp.kt
@@ -1,6 +1,13 @@
 package com.nillsmaillet.firepitapplication.ui
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -10,13 +17,29 @@ import com.nillsmaillet.firepitapplication.ui.navigation.Destination
 fun FirepitApp (){
     val navController = rememberNavController()
 
-    NavHost(
-        navController = navController,
-        startDestination = Destination.LIBRARY.route,
-    ){
-        composable (Destination.LIBRARY.route){ PlaceholderScreen(Destination.LIBRARY.label)}
-        composable (Destination.JOURNAL.route){ PlaceholderScreen(Destination.JOURNAL.label)}
-        composable (Destination.STATS.route){ PlaceholderScreen(Destination.STATS.label)}
-        composable (Destination.SETTINGS.route){ PlaceholderScreen(Destination.SETTINGS.label)}
+    Scaffold(
+        bottomBar = {
+            NavigationBar() {
+                Destination.entries.forEach { destination ->
+                    NavigationBarItem(
+                        selected = false,
+                        onClick = {},
+                        icon = { Icon(destination.icon, contentDescription = destination.label) },
+                        label = { Text(destination.label) }
+                    )
+                }
+            }
+        }
+    ) { innerPadding ->
+        NavHost(
+            navController = navController,
+            startDestination = Destination.LIBRARY.route,
+            modifier = Modifier.padding(innerPadding)
+        ){
+            composable (Destination.LIBRARY.route){ PlaceholderScreen(Destination.LIBRARY.label)}
+            composable (Destination.JOURNAL.route){ PlaceholderScreen(Destination.JOURNAL.label)}
+            composable (Destination.STATS.route){ PlaceholderScreen(Destination.STATS.label)}
+            composable (Destination.SETTINGS.route){ PlaceholderScreen(Destination.SETTINGS.label)}
+        }
     }
 }
